9906a84e2da438b69d021ed0db221067184d86af
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-25 18:49:13 +0100

    Completed MVP and Extension.

--- answers.sql
+++ answers.sql
@@ -8,8 +8,16 @@ SELECT name FROM people WHERE id = 1;
 
 DELETE FROM movies WHERE title = 'Batman Begins';
 
--- INSERT INTO people (name) VALUES ('Bart Simpson');
+INSERT INTO people (name) VALUES ('Bart Simpson');
 
 DELETE FROM people WHERE name = 'Eric Cartman';
 
-INSERT INTO movies (title) VALUES ('Avengers: Infinity War!!');
+INSERT INTO movies (title) VALUES ('Avengers: Infinity War!!'); 
+
+SELECT show_time FROM movies WHERE title = 'Iron Man 2';
+
+UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
+
+DELETE FROM people WHERE id >= 2 AND id <= 6;
+
+DELETE FROM movies WHERE id BETWEEN 2 AND 6
