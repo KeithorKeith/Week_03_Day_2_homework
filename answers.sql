16f627f229e120663f804e3042570980d32219f3
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-25 18:29:19 +0100

    Initial commit.

---
+++ answers.sql
@@ -0,0 +1,15 @@
+SELECT * FROM movies;
+
+SELECT name FROM people;
+
+UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+
+SELECT name FROM people WHERE id = 1;
+
+DELETE FROM movies WHERE title = 'Batman Begins';
+
+-- INSERT INTO people (name) VALUES ('Bart Simpson');
+
+DELETE FROM people WHERE name = 'Eric Cartman';
+
+INSERT INTO movies (title) VALUES ('Avengers: Infinity War!!');
